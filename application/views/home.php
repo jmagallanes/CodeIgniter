06764c8f40dfcb97b62e4b5bd5fc84470b9a2156
Jeisson Magallanes <jeissonmagallanes9@gmail.com>
2014-12-18 19:42:44 -0500

    Se ha quitado el slash duplicado en la relación de llamada de un archivo js y css (//public por /public)

--- application/views/home.php
+++ application/views/home.php
@@ -4,15 +4,15 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<title>Welcome to CodeIgniter by Jeisson</title>
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>/public/css/normalize.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>public/css/normalize.css">
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>/public/css/style.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>public/css/style.css">
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<!--[if lt IE 9]>
-        	<script src="<?php echo base_url(); ?>/public/js/html5shiv.min.js"></script>
+        	<script src="<?php echo base_url(); ?>public/js/html5shiv.min.js"></script>
         <![endif]-->
-        <script src="<?php echo base_url(); ?>/public/js/functions.js"></script>
+        <script src="<?php echo base_url(); ?>public/js/functions.js"></script>
 	</head>
 	<body>
 		<header>
